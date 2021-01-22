b7b7bcdffd18c8a1772b7b97db63d0de637f825d
Pinky <janeletageng28@gmail.com>
2021-01-22 17:52:13 +0200

    Fix url error

--- Covid/urls.py
+++ Covid/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('covid_19.urls'),
+    path('', include('covid_19.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
