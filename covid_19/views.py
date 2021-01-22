36f6f67c219dd92beb73e5ce22d8b2071b66c6d7
Pinky <janeletageng28@gmail.com>
2021-01-22 19:43:49 +0200

    Removed Pandas

--- covid_19/views.py
+++ covid_19/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
 
 df3=pd.read_json('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/world-population-density.json')
 def indexPage(request):
